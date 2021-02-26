a2bf526722bf0ad8b9a89e43c7f9297ddca4ceac
1008611 <1008611@.com>
2021-02-26 16:06:05 +0800

    配置路由

--- demo/src/components/Login.vue
+++ demo/src/components/Login.vue
@@ -6,12 +6,27 @@
         <img src="./../assets/logo.png" alt="" />
       </div>
       <!-- 登录表单 -->
-      <el-form label-width="80px" class="login-form" :model='form' :rules="loginRules" ref='loginForm'>
-        <el-form-item label="用户账号" prop='username'>
-          <el-input placeholder="请输入用户名" v-model="form.username" prefix-icon="el-icon-user"></el-input>
+      <el-form
+        label-width="80px"
+        class="login-form"
+        :model="form"
+        :rules="loginRules"
+        ref="loginForm"
+      >
+        <el-form-item label="用户账号" prop="username">
+          <el-input
+            placeholder="请输入用户名"
+            v-model="form.username"
+            prefix-icon="el-icon-user"
+          ></el-input>
         </el-form-item>
-        <el-form-item label="用户密码" prop='password'>
-          <el-input type="password" placeholder="请输入用户密码" v-model="form.password" prefix-icon="el-icon-s-custom"></el-input>
+        <el-form-item label="用户密码" prop="password">
+          <el-input
+            type="password"
+            placeholder="请输入用户密码"
+            v-model="form.password"
+            prefix-icon="el-icon-s-custom"
+          ></el-input>
         </el-form-item>
         <div class="btns">
           <el-button type="primary" @click="submit">提交</el-button>
@@ -28,42 +43,60 @@ export default {
     return {
       form: {
         username: "",
-        password:""
+        password: "",
       },
       // 表单验证规则
       loginRules: {
         username: [
-          { required:true,message:'请输入用户账号',trigger: 'blur' },
-          { min:4,max:14,message:'用户账号长度为6-14个字符',trigger: 'blur' }
+          { required: true, message: "请输入用户账号", trigger: "blur" },
+          {
+            min: 4,
+            max: 14,
+            message: "用户账号长度为6-14个字符",
+            trigger: "blur",
+          },
         ],
         password: [
-          { required:true,message:'请输入用户密码',trigger: 'blur' },
-          { min:6,max:10,message:'用户密码长度为6-14个字符',trigger: 'blur' }
-        ]
-      }
+          { required: true, message: "请输入用户密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 10,
+            message: "用户密码长度为6-14个字符",
+            trigger: "blur",
+          },
+        ],
+      },
     };
   },
-  methods:{
-    submit(){
-      this.$refs.loginForm.validate((valid)=>{
-        if(valid){
-          console.log(this.form)
-          setTimeout(()=>{
-            if(this.form.username === 'admin' && this.form.password === '123456'){
-              alert("登陆成功")
-            }else{
-              alert("账号或者密码有误")
+  methods: {
+    submit() {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          setTimeout(() => {
+            if (
+              this.form.username === "admin" &&
+              this.form.password === "123456"
+            ) {
+              this.$message({
+                message: "登录成功",
+                type: "success",
+              });
+              var token = 'UJHHJuiuq543F54SD45FDG';
+              sessionStorage.setItem('token',token);
+              this.$router.push('/home');
+            } else {
+               this.$message.error('账号或密码错误');
             }
-          },800)
-        }else{
-          return
+          }, 100);
+        } else {
+          return;
         }
-      })
+      });
     },
-    reset(){
+    reset() {
       this.$refs.loginForm.resetFields();
-    }
-  }
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -93,15 +126,15 @@ export default {
         background: #ddd;
       }
     }
-    .login-form{
+    .login-form {
       margin-top: 80px;
-      .btns{
+      .btns {
         text-align: center;
       }
     }
   }
 }
-.el-form-item{
+.el-form-item {
   padding: 0 25px;
 }
 </style>
--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -1,17 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
-const Login = () => import('./../components/Login.vue')
+const Login = () => import('./../components/Login.vue');
+const Home = () => import('./../components/Home.vue');
 
 const routes = [
   { path: '/', redirect: '/login' },
-  { path: "/login", component: Login }
-]
+  { path: "/login", component: Login },
+  { path: "/home", component: Home, meta: { checkLogin: true } }
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
+// 路由前置守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === "/login") {
+    sessionStorage.getItem("token")?next('/home'):next();
+  }else{
+    sessionStorage.getItem("token")?next():next('/login');
+  };
+});
 export default router
